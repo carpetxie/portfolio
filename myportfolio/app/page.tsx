51c0c752af3defad7c73b33f68862333618ee7ac
Jeffrey Xie <jeffreyxiekl@gmail.com>
2026-03-25 13:19:49 -0400

    new info

--- myportfolio/app/page.tsx
+++ myportfolio/app/page.tsx
@@ -158,9 +158,9 @@ export default function Portfolio() {
             <p>
 
 
-            I study math/cs at Dartmouth College. My research spans activation steering and information theory. I was formerly a research engineer at <a href= "https://www.adaptional.com/" className="italic underline">Adaptional (YC S25)</a> and a gardener at <a href = "https://www.jacqueminfarms.net/" className="italic underline">Jacquemin Farms</a>. Feel free to check out my <a href = "https://www.adaptional.com/blog/foundational-models-hit-94-on-insurance-docs" className= "italic underline">work.</a> </p>
+            I study math/cs at Dartmouth College. My research spans activation steering and mechanistic interpretability. I was formerly a research engineer at <a href= "https://www.adaptional.com/" className="italic underline">Adaptional (YC S25)</a> and a gardener at <a href = "https://www.jacqueminfarms.net/" className="italic underline">Jacquemin Farms</a>. Feel free to check out my <a href = "https://www.adaptional.com/blog/foundational-models-hit-94-on-insurance-docs" className= "italic underline">work.</a> </p>
             <p>
-            My current focus is to find a “prime composition” of basis vectors that compose personality. I&apos;m also working on quantifying context value for AI agents with my dear friend <a href="https://praneelp.me/" target="_blank" rel="noopener noreferrer" className="italic underline">Praneel Patel</a>.
+            My current focus is to find a “prime composition” of basis vectors that compose personality. I'm also working on some cool stuff with my dear friend <a href="https://praneelp.me/" target="_blank" rel="noopener noreferrer" className="italic underline">Praneel Patel</a>.
             </p>
             <p>
             In my free time,  I enjoy thinking and writing in my digital journal, distance running(17:29 5k many years ago), weightlifting, performing chamber violin music and minimalistic photography. 
